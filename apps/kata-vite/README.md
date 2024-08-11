54e39bebb30e4fce13914a72c907b27e2ba12fb9
David Diaz <diazdavid.info@gmail.com>
2024-08-11 10:51:18 +0200

    new projects

---
+++ apps/kata-vite/README.md
@@ -0,0 +1,5 @@
+https://ui.shadcn.com/
+https://vitejs.dev/guide/
+https://www.npmjs.com/package/fast-xml-parser
+https://grrr.tech/posts/create-dom-node-from-html-string/
+https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issues/#api-rest-api-3-issue-post
--- apps/stripe/src/index.ts
+++ apps/stripe/src/index.ts
@@ -2,13 +2,14 @@ import path from 'path'
 import { apiRouter } from 'api-route'
 import * as fs from 'node:fs'
 import Stripe from 'stripe'
+import { setTimeout } from 'node:timers/promises'
 
 const stripeSecretKey =
   'sk_test_51KsNaFK5jvkJ3LGPod0kj11lXHBb6c38V5M2XMoXA5zyoTpoUCuSTS40wLjt1yHtDRm3HDVctuuTGpvOmfaxQ1bY00WbKYcti7'
 
 const app = apiRouter()
 
-app.add('GET', '/', () => {
+app.add('GET', '/', async () => {
   const pathFile = path.join(__dirname, 'views', 'home.html')
   const contentFile = fs.readFileSync(pathFile, 'utf8')
 
@@ -18,19 +19,21 @@ app.add('GET', '/', () => {
 app.add('GET', '/check', async ({ request }) => {
   const query = new URL(request.url).searchParams
   const sessionId = query.get('session_id')
+  const callback = query.get('callback')
 
   const stripe = new Stripe(stripeSecretKey)
 
   const session = await stripe.checkout.sessions.retrieve(sessionId as string)
 
-  return new Response(JSON.stringify(session))
+  return new Response(JSON.stringify({ session, callback }))
 })
 
 app.add('POST', '/init', async () => {
   const stripe = new Stripe(stripeSecretKey)
 
   const session = await stripe.checkout.sessions.create({
-    success_url: 'http://localhost:3000/check?session_id={CHECKOUT_SESSION_ID}',
+    success_url: 'http://localhost:3000/check?session_id={CHECKOUT_SESSION_ID}&callback=https://google.es',
+    cancel_url: 'http://localhost:3000',
     line_items: [
       {
         price_data: {
@@ -38,17 +41,22 @@ app.add('POST', '/init', async () => {
           product_data: {
             name: 'Example Product',
             description: 'Example Product Description',
-            images: []
+            images: ['https://storage.googleapis.com/elevate-production-bucket/space/5db03ec6e9464fe92ccf600040538da7']
           },
           unit_amount: 1000
         },
-        quantity: 1
+        quantity: 10
       }
     ],
     mode: 'payment'
   })
 
+  // const { url } = session
+
+  console.log(session)
+
   return new Response(JSON.stringify(session))
+  // return new Response('Redirect', { status: 302, headers: { Location: url as string } })
 })
 
 app.run(3000, () => {
