8a5150a907c51ac9c99196da3c1598d110e6c125
David Diaz <diazdavid.info@gmail.com>
2024-11-27 13:24:29 +0100

    add functions

--- apps/calendar-microsoft-graph/search-instances.ts
+++ apps/calendar-microsoft-graph/search-instances.ts
@@ -1,6 +1,7 @@
 import { Client } from "@microsoft/microsoft-graph-client";
 
 const { MICROSOFT_TOKEN, MICROSOFT_CALENDAR_ID } = process.env;
+const eventId = "";
 
 const getClientByToken = (token: string): Client => {
   const graphOptions = { authProvider: async (done: any) => done(null, token) };
@@ -10,7 +11,7 @@ const getClientByToken = (token: string): Client => {
 const graphClient = getClientByToken(MICROSOFT_TOKEN as string);
 graphClient
   .api(
-    `/me/calendars/${MICROSOFT_CALENDAR_ID}/events?$filter=start/dateTime ge '2024-11-30T00:00:00Z'`,
+    `/me/calendars/${MICROSOFT_CALENDAR_ID}/events/${eventId}/instances?$top=10&startDateTime=2024-11-27T00:00:00.0000000&endDateTime=2025-11-26T00:00:00.0000000`,
   )
   .get()
   .then((response) => console.log(JSON.stringify(response)))
