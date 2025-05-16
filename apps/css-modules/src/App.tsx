d19e4a2621a2caac27c786f101b0d82bad28325d
David Diaz <diazdavid.info@gmail.com>
2025-05-16 18:33:08 +0200

    new project

---
+++ apps/css-modules/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Typography from "./Typography.tsx";
+
+function App() {
+  return (
+    <>
+      <Typography size={16}>Hola</Typography>
+      <Typography size={20}>Hola</Typography>
+      <Typography size={24}>Hola</Typography>
+      <Typography size={28}>Hola</Typography>
+      <Typography size={32}>Hola</Typography>
+      <Typography size={36}>Hola</Typography>
+      <Typography size={40}>Hola</Typography>
+    </>
+  )
+}
+
+export default App
---
+++ apps/css-modules/src/Typography.tsx
@@ -0,0 +1,15 @@
+import type {CSSProperties, ElementType, PropsWithChildren} from 'react'
+import './Typography.css'
+
+type TypographyProps = {
+  size: number
+  as?: ElementType;
+}
+
+export default function Typography({size, children, as: Component = 'h1'}: PropsWithChildren<TypographyProps>) {
+  const style: CSSProperties = {
+    "--font-size": `${size}px`
+  }
+
+  return <Component style={style}>{children}</Component>
+}
