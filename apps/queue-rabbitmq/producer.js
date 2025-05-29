33f9defd422ac08a3bd279c8a5a11d9f8f21cb0e
David Diaz <diazdavid.info@gmail.com>
2025-05-29 09:35:54 +0200

    new project

---
+++ apps/queue-rabbitmq/producer.js
@@ -0,0 +1,13 @@
+import {connect} from "amqplib";
+import process from "eslint-config-mytools";
+
+const queue = 'tasks';
+const conn = await connect(process.env.RABBIT_URL)
+
+const ch2 = await conn.createChannel();
+
+let count = 0;
+
+setInterval(() => {
+  ch2.sendToQueue(queue, Buffer.from(`something to do ${++count}`));
+}, 1000);
