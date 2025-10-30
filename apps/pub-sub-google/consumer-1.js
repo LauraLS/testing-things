81e1697f651c318733ebfe9c85012e7ee7060a96
David Diaz <diazdavid.info@gmail.com>
2025-10-30 16:01:15 +0100

    new project

---
+++ apps/pub-sub-google/consumer-1.js
@@ -0,0 +1,21 @@
+import {PubSub} from '@google-cloud/pubsub';
+import {googleAuth} from "./auth";
+
+const main = async () => {
+  const { GC_PROJECT_ID } = process.env
+
+  const pubsub = new PubSub({
+    auth: googleAuth(),
+    projectId: GC_PROJECT_ID,
+  })
+  const topic = await pubsub.topic('topic-pub_sub')
+  const subscription = await topic.subscription('subscription-1')
+
+  subscription.on('message', message => {
+    const {data} = message
+    console.log(`Consumer 1: ${data.toString()}`);
+    message.ack()
+  });
+}
+
+main().then(console.log).catch(console.error)
